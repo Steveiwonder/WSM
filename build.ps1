58c3c96f7ff401ce1ba97488b0af404d4e98ce24
Steven Yates <s_yates1@hotmail.com>
2023-08-09 13:59:56 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,15 +1,16 @@
 Write-Host "Building WSM.Server"
-$verison = "$Env:VERSION_NUMBER"
+gci env:* | sort-object name
+$verison = "$env:VERSION_NUMBER"
 Write-Host "Version number $version"
-docker build -t steveiwonder/wsm.server:$version -t steveiwonder/wsm.server:latest --no-cache --progress=plain -f dockerfile.server .
-Write-Host "Publishing WSM.Client"
-dotnet publish "./WSM.Client/WSM.Client.csproj" -o ./build/wsm.client/ -c Debug
-Write-Host "Removing client appsettings.json"
-Remove-Item -Path ./build/wsm.client/appsettings.json -ErrorAction Ignore
-Write-Host "Copying install-service.ps1"
-Copy-Item -Path ./WSM.Client/install-service.ps1 ./build/wsm.client/install-service.ps1
-Write-Host "Copying uninstall-service.ps1"
-Copy-Item -Path ./WSM.Client/uninstall-service.ps1 ./build/wsm.client/uninstall-service.ps1
-Write-Host "Creating wsm.client.zip"
-Compress-Archive -Path ./build/wsm.client -DestinationPath ./build/wsm.client.zip -Force
-Write-Host "Done"
+#docker build -t steveiwonder/wsm.server:$version -t steveiwonder/wsm.server:latest --no-cache --progress=plain -f dockerfile.server .
+#Write-Host "Publishing WSM.Client"
+#dotnet publish "./WSM.Client/WSM.Client.csproj" -o ./build/wsm.client/ -c Debug
+#Write-Host "Removing client appsettings.json"
+#Remove-Item -Path ./build/wsm.client/appsettings.json -ErrorAction Ignore
+#Write-Host "Copying install-service.ps1"
+#Copy-Item -Path ./WSM.Client/install-service.ps1 ./build/wsm.client/install-service.ps1
+#Write-Host "Copying uninstall-service.ps1"
+#Copy-Item -Path ./WSM.Client/uninstall-service.ps1 ./build/wsm.client/uninstall-service.ps1
+#Write-Host "Creating wsm.client.zip"
+#Compress-Archive -Path ./build/wsm.client -DestinationPath ./build/wsm.client.zip -Force
+#Write-Host "Done"
